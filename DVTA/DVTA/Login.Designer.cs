ea987f391bee05f057e7a7c867839e7dcc515e4a
surya <surya82@users.noreply.github.com>
2017-10-29 22:54:18 -0400

    SSL Pinning

--- DVTA/DVTA/Login.Designer.cs
+++ DVTA/DVTA/Login.Designer.cs
@@ -36,6 +36,8 @@
             this.btnlogin = new System.Windows.Forms.Button();
             this.btnregister = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lgnusername
@@ -103,12 +105,34 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "Login Here";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Fetch Time";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "current IST Time";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(526, 399);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnregister);
             this.Controls.Add(this.btnlogin);
@@ -135,5 +159,7 @@
         private System.Windows.Forms.Button btnlogin;
         private System.Windows.Forms.Button btnregister;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- DVTA/DVTA/Login.cs
+++ DVTA/DVTA/Login.cs
@@ -9,7 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using DBAccess;
-
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 namespace DVTA
 {
@@ -124,6 +126,31 @@ namespace DVTA
             
         }
 
-       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServicePointManager.ServerCertificateValidationCallback = PinPublicKey;
+            WebRequest wr = WebRequest.Create("https://time.is/Unix_time_nows");
+            WebResponse timeResp=wr.GetResponse();
+            label1.Text = Convert.ToString(timeResp.ContentLength);
+        }
+        private static String PUB_KEY = "3082010A0282010100B9A66237F88CC35C385712F002B469704B007A624A9862289926361D46524B589FD5B198BE557D48ECE7EAAC270531B5072030BC04698CE560B84C6E51FE2000EA5FC605D89F6E2C11581B416DCB3B71DE1B1526ABE48B342469C618E9BDA5E68A50655AC96B05E35B1955A3DB9ADF48F32A9F55B836B8EA89BB818094B2B549A5DD37C7F6CA879E0C40EB64F6E5F4B5152169BFD068EF488A5611BD9E37FB15CC2741F1D6F719567775F3333EFCBB9B659E9033FDE01C2B32543EF7E072897FA7CF79EBBEBA9C005A1F0DAD0F7CD7F025FBA3782FD34FF362B97868273F2A9E05CE4AF872DFAEE6F40A83F76348DAC54F710C82B5DFC2141AC2DDABECF1361D0203010001";
+        public static bool PinPublicKey(object sender, X509Certificate certificate, X509Chain chain,
+                                SslPolicyErrors sslPolicyErrors)
+        {
+            if (null == certificate)
+                return false;
+
+            String pk = certificate.GetPublicKeyString();
+            if (pk.Equals(PUB_KEY))
+                return true;
+
+            // Bad dog
+            return false;
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
